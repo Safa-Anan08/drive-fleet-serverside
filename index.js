6252b2d1bea0d7e1f358dd415aee33811ff0298e
Safa-Anan08 <safaspractice@gmail.com>
2026-05-19 12:17:32 +0600

    clean commit without secrets

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import authRoutes from "./routes/authRoutes.js";
+import carRoutes from "./routes/carRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+  })
+);
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/cars", carRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port", process.env.PORT);
+});
